c074c5efed57fcc48551b16d9d822d8e9ff8047b
raczboldizsar <raboldizsar97@gmail.com>
2019-02-18 07:46:45 -0800

    bug fixed and test added

--- train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
+++ train-system/src/test/java/hu/bme/mit/train/system/TrainSystemTest.java
@@ -50,5 +50,13 @@ public class TrainSystemTest {
 		Assert.assertEquals(0, controller.getReferenceSpeed());
 	}
 
-	
+	@Test
+	public void newTest(){
+		user.overrideJoystickPosition(100);
+		controller.followSpeed();
+		controller.emergencyBrake();
+		Assert.assertEquals(0, controller.getReferenceSpeed());
+	}
+}
+    
 }
